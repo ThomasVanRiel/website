347a4435ffaf2f9fbe0b234c6a01d8b06389ef59
Thomas Van Riel <thomas.vanriel@kuleuven.be>
2025-02-25 11:16:19 +0100

    Styling

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -39,8 +39,8 @@ export default function Blog({ data, tags }: Props) {
           <ul class="flex flex-wrap sm:flex-col gap-1.5">
             <For each={tags}>
               {(tag) => (
-                <li class="bg-white dark:bg-black">
-                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded ", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-black/5 dark:bg-white/10", "hover:bg-black/10 hover:dark:bg-white/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
+                <li class="bg-brand-lt dark:bg-brand-dk">
+                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded ", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-brand-dk/5 dark:bg-brand-lt/10", "hover:bg-brand-dk/10 hover:dark:bg-brand-lt/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
                     <svg class={cn("size-5 fill-black/50 dark:fill-white/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-black dark:fill-white")}>
                       <use href={`/ui.svg#square`} class={cn(!filter().has(tag) ? "block" : "hidden")} />
                       <use href={`/ui.svg#square-check`} class={cn(filter().has(tag) ? "block" : "hidden")} />
@@ -60,7 +60,7 @@ export default function Blog({ data, tags }: Props) {
           </div>
           <ul class="flex flex-col gap-3">
             {posts().map((post) => (
-              <li class="bg-white dark:bg-black">
+              <li class="bg-brand-lt dark:bg-brand-dk">
                 <ArrowCard entry={post} />
               </li>
             ))}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme"
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,6 +7,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors:{
+        "brand-dk":  colors.slate['800'],
+        "brand-lt": colors.stone['100'],
+        "brand-accent-dk": colors.slate['400'],
+        "brand-accent-lt": colors.stone['400'],
+      },
       fontFamily: {
         sans: ['"Source Sans 3"', ...defaultTheme.fontFamily.sans],
         serif: ['"Source Serif 4"', ...defaultTheme.fontFamily.serif],
