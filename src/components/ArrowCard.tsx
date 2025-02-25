d1a9e98657eed1bf7c26a9fa4c7b826223f52eb9
Thomas Van Riel <thomas.vanriel@kuleuven.be>
2025-02-25 11:26:48 +0100

    Remove unnecessary files and speed up animation

--- src/components/ArrowCard.tsx
+++ src/components/ArrowCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ArrowCard({entry, pill}: Props) {
     return (
-      <a href={`/${entry.collection}/${entry.slug}`} class="group p-4 gap-3 flex items-center border rounded-lg hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-colors duration-200 ease-in-out">
+      <a href={`/${entry.collection}/${entry.slug}`} class="group p-4 gap-3 flex items-center border rounded-lg hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-colors duration-100 ease-in-out">
       <div class="w-full group-hover:text-brand-dk group-hover:dark:text-brand-lt blend ">
         <div class="flex flex-wrap items-center gap-2">
           {pill &&
@@ -36,8 +36,8 @@ export default function ArrowCard({entry, pill}: Props) {
         </ul>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="stroke-current group-hover:stroke-black group-hover:dark:stroke-white">
-        <line x1="5" y1="12" x2="19" y2="12" class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-200 ease-in-out" />
-        <polyline points="12 5 19 12 12 19" class="translate-x-0 group-hover:translate-x-1 transition-all duration-200 ease-in-out" />
+        <line x1="5" y1="12" x2="19" y2="12" class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-100 ease-in-out" />
+        <polyline points="12 5 19 12 12 19" class="translate-x-0 group-hover:translate-x-1 transition-all duration-100 ease-in-out" />
       </svg>
     </a>
    )
