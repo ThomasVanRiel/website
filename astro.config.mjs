9e8e760a324dfa54efcc33e38d7cd30b1f3e22b3
Thomas Van Riel <thomas.vanriel@kuleuven.be>
2025-12-27 21:15:29 +0100

    add math

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import remarkMath from "remark-math"
+import rehypeMathjax from "rehype-mathjax"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thomasvanriel.com",
-  integrations: [mdx(), sitemap({
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  }), sitemap({
       filter: (page) => {
         // Filter out known draft posts
         const draftPaths = [
